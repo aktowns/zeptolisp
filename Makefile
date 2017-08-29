a14183b46c3c69e7c6e6a6900b90e293c70d0595
Michael Vigilante <vigilante.michael@gmail.com>
2017-08-29 21:13:04 +1000

    Move the 'eval' stuff into its own file

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@ CFLAGS=-lreadline -g
 %.o: %.c
 	clang ${CFLAGS} -c -o $@ $<
 
-zepto: src/main.o src/ast.o src/parser.o
-	clang ${CFLAGS} -o zepto src/main.o src/ast.o src/parser.o
+zepto: src/main.o src/ast.o src/parser.o src/eval.o
+	clang ${CFLAGS} -o zepto src/main.o src/ast.o src/parser.o src/eval.o
 
-clean: 
+clean:
 	rm -vf src/*.o
 
---
+++ src/eval.h
@@ -0,0 +1,32 @@
+#ifndef __EVAL_H
+#define __EVAL_H
+
+#include "ast.h"
+
+#define IS_TYPE(node, typ) (node->type == typ)
+#define HEAD(node) (node->value.list->car)
+
+#define AS_STRING(node) (node->value.string->value)
+#define AS_NUMBER(node) (node->value.number->value)
+
+typedef node_t *(*fn)(ast_node_list_t*);
+
+typedef struct {
+    char* name;
+    union {
+        fn function;
+        node_t* data;
+    } value;
+} env_t;
+
+node_t* evaluateList(env_t*[], node_t*);
+node_t* evaluate(env_t*[], node_t*);
+node_t* lispPrint(ast_node_list_t*);
+node_t* lispAdd(ast_node_list_t*);
+
+static env_t print = {.name = "print", .value.function = &lispPrint};
+static env_t add = {.name = "+", .value.function = &lispAdd};
+
+static env_t* builtins[] = { &print, &add, NULL };
+
+#endif
