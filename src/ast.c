5de8da0b33934364057fa85cb854476c5ef0d81a
Michael Vigilante <vigilante.michael@gmail.com>
2017-09-03 20:44:58 +1000

    Re-indent to two rather than four spaces

--- src/ast.c
+++ src/ast.c
@@ -6,153 +6,153 @@
 #include "ast.h"
 
 node_t* mkNode(ast_type_t type) {
-    node_t* node = malloc(sizeof(node_t));
-    node->type = type;
-    return node;
+  node_t* node = malloc(sizeof(node_t));
+  node->type = type;
+  return node;
 }
 
 node_t* mkNodeString(char* str) {
-    node_t* node = mkNode(AST_STRING);
+  node_t* node = mkNode(AST_STRING);
 
-    ast_node_string_t* nodeStr = malloc(sizeof(ast_node_string_t));
-    nodeStr->value = malloc(strlen(str)+1);
-    strcpy(nodeStr->value, str);
+  ast_node_string_t* nodeStr = malloc(sizeof(ast_node_string_t));
+  nodeStr->value = malloc(strlen(str)+1);
+  strcpy(nodeStr->value, str);
 
-    node->value.string = nodeStr;
-    return node;
+  node->value.string = nodeStr;
+  return node;
 }
 
 node_t* mkNodeSymbol(char* str) {
-    node_t* node = mkNode(AST_SYMBOL);
+  node_t* node = mkNode(AST_SYMBOL);
 
-    ast_node_symbol_t* nodeStr = malloc(sizeof(ast_node_symbol_t));
-    nodeStr->value = malloc(strlen(str)+1);
-    strcpy(nodeStr->value, str);
+  ast_node_symbol_t* nodeStr = malloc(sizeof(ast_node_symbol_t));
+  nodeStr->value = malloc(strlen(str)+1);
+  strcpy(nodeStr->value, str);
 
-    node->value.symbol = nodeStr;
-    return node;
+  node->value.symbol = nodeStr;
+  return node;
 }
 
 node_t* mkNodeNumber(int64_t num) {
-    node_t* node = mkNode(AST_NUMBER);
+  node_t* node = mkNode(AST_NUMBER);
 
-    ast_node_number_t* nodeNum = malloc(sizeof(ast_node_number_t));
-    nodeNum->value = num;
+  ast_node_number_t* nodeNum = malloc(sizeof(ast_node_number_t));
+  nodeNum->value = num;
 
-    node->value.number = nodeNum;
-    return node;
+  node->value.number = nodeNum;
+  return node;
 }
 
 node_t* mkQuoted(node_t* child) {
-    node_t* node = mkNode(AST_QUOTED);
+  node_t* node = mkNode(AST_QUOTED);
 
-    ast_node_quoted_t* quoted = malloc(sizeof(ast_node_quoted_t));
-    quoted->inner = child;
+  ast_node_quoted_t* quoted = malloc(sizeof(ast_node_quoted_t));
+  quoted->inner = child;
 
-    node->value.quoted = quoted;
-    return node;
+  node->value.quoted = quoted;
+  return node;
 }
 
 ast_node_list_t* cons(node_t* car, ast_node_list_t* cdr) {
-    ast_node_list_t* list = malloc(sizeof(ast_node_list_t));
-    list->car = car;
-    list->cdr = cdr;
-    return list;
+  ast_node_list_t* list = malloc(sizeof(ast_node_list_t));
+  list->car = car;
+  list->cdr = cdr;
+  return list;
 }
 
 ast_node_list_t* append(node_t* value, ast_node_list_t* list) {
-    if (list == NULL) return cons(value, &nil);
+  if (list == NULL) return cons(value, &nil);
 
-    ast_node_list_t* cell = malloc(sizeof(ast_node_list_t));
-    cell->car = value;
-    cell->cdr = &nil;
+  ast_node_list_t* cell = malloc(sizeof(ast_node_list_t));
+  cell->car = value;
+  cell->cdr = &nil;
 
-    if (IS_NIL(list)) {
-        return cell;
+  if (IS_NIL(list)) {
+    return cell;
+  }
+
+  ast_node_list_t* node = NULL;
+  ast_node_list_t* parent = list;
+  while ((node = parent->cdr)) {
+    if (IS_NIL(node)) {
+      parent->cdr = cell;
+      return list;
     }
+    parent = node;
+  }
+  // We didn't find Nil at the end of a list.
+  free(cell);
+  fprintf(stderr, "list append failed.");
 
-    ast_node_list_t* node = NULL;
-    ast_node_list_t* parent = list;
-    while ((node = parent->cdr)) {
-        if (IS_NIL(node)) {
-            parent->cdr = cell;
-            return list;
-        }
-        parent = node;
-    }
-    // We didn't find Nil at the end of a list.
-    free(cell);
-    fprintf(stderr, "list append failed.");
-
-    return &nil;
+  return &nil;
 }
 
 ast_node_list_t* tail(ast_node_list_t* list) {
-    return list->cdr;
+  return list->cdr;
 }
 
 node_t* wrapNodeList(ast_node_list_t* list) {
-    node_t* node = mkNode(AST_LIST);
-    node->value.list = list;
-    return node;
+  node_t* node = mkNode(AST_LIST);
+  node->value.list = list;
+  return node;
 }
 
 void _nodePP(node_t* node) {
-    if (node == NULL) return;
+  if (node == NULL) return;
 
-    switch(node->type) {
-        case AST_STRING: 
-            printf("<STRING \"%s\">", node->value.string->value);
-            break;
-        case AST_NUMBER: 
-            printf("<NUMBER %ld>", node->value.number->value);
-            break;
-        case AST_LIST: 
-            if(IS_NIL(node->value.list)) {
-                printf("nil");
-            } else {
-                printf("(");
-                listPP(node->value.list);
-                printf(")");
-            }
-            break;
-        case AST_SYMBOL:
-            printf("<SYMBOL %s>", node->value.symbol->value);
-            break;
-        case AST_QUOTED:
-            printf("'");
-            _nodePP(node->value.quoted->inner);
-            break;
-        default:
-            puts("<UNKNOWN>"); 
+  switch(node->type) {
+  case AST_STRING:
+    printf("<STRING \"%s\">", node->value.string->value);
+    break;
+  case AST_NUMBER:
+    printf("<NUMBER %ld>", node->value.number->value);
+    break;
+  case AST_LIST:
+    if(IS_NIL(node->value.list)) {
+      printf("nil");
+    } else {
+      printf("(");
+      listPP(node->value.list);
+      printf(")");
     }
+    break;
+  case AST_SYMBOL:
+    printf("<SYMBOL %s>", node->value.symbol->value);
+    break;
+  case AST_QUOTED:
+    printf("'");
+    _nodePP(node->value.quoted->inner);
+    break;
+  default:
+    puts("<UNKNOWN>");
+  }
 }
 
 void nodePP(node_t* node) {
-    printf("=> ");
-    _nodePP(node);
+  printf("=> ");
+  _nodePP(node);
 }
 
 void listPP(ast_node_list_t* list) {
-    if (list == NULL) return;
+  if (list == NULL) return;
 
-    _nodePP(list->car);
+  _nodePP(list->car);
 
-    if (!IS_NIL(list->cdr)) {
-        printf(" ");
-        listPP(list->cdr);
-    } 
-} 
+  if (!IS_NIL(list->cdr)) {
+    printf(" ");
+    listPP(list->cdr);
+  }
+}
 
 const char* strType(node_t* node) {
-    if (node == NULL) return NULL;
+  if (node == NULL) return NULL;
 
-    switch(node->type) {
-        case AST_SYMBOL: return "Symbol";
-        case AST_LIST: return "List";
-        case AST_NUMBER: return "Number";
-        case AST_STRING: return "String";
-        case AST_QUOTED: return "Quote";
-        default: return "Unknown";
-    }
+  switch(node->type) {
+  case AST_SYMBOL: return "Symbol";
+  case AST_LIST: return "List";
+  case AST_NUMBER: return "Number";
+  case AST_STRING: return "String";
+  case AST_QUOTED: return "Quote";
+  default: return "Unknown";
+  }
 }
--- src/parser.c
+++ src/parser.c
@@ -3,168 +3,168 @@
 #include <ctype.h>
 #include <stdio.h>
 
-#define MAX_NUMBER_WIDTH 25 
+#define MAX_NUMBER_WIDTH 25
 parser_result_t* parseNumber(const char* input) {
-    parser_result_t* res = malloc(sizeof(parser_result_t));
-    char c;
+  parser_result_t* res = malloc(sizeof(parser_result_t));
+  char c;
 
-    char* bfr = calloc(sizeof(char), MAX_NUMBER_WIDTH);
-    int z = 0;
-    for (; isdigit(c = input[z]) || c == '-'; z++) {
-        bfr[z] = c;
-    }
-    node_t* node = mkNodeNumber(atoi(bfr));
-    free(bfr);
+  char* bfr = calloc(sizeof(char), MAX_NUMBER_WIDTH);
+  int z = 0;
+  for (; isdigit(c = input[z]) || c == '-'; z++) {
+    bfr[z] = c;
+  }
+  node_t* node = mkNodeNumber(atoi(bfr));
+  free(bfr);
 
-    res->consumed = z;
-    res->value = node;
-    return res;
+  res->consumed = z;
+  res->value = node;
+  return res;
 }
 
 parser_result_t* parseString(const char* input) {
-    parser_result_t* res = malloc(sizeof(parser_result_t));
-    char c;
+  parser_result_t* res = malloc(sizeof(parser_result_t));
+  char c;
 
-    char* bfr = calloc(sizeof(char), 1024);
-    int z = 1;
-    for (; (c = input[z]) != '"'; z++) {
-        bfr[z-1] = c;
-        if (z % 1023 == 0) {
-            int sz = strlen(bfr)*sizeof(char);
-            bfr = realloc(bfr, sz + 1024);
-            memset(bfr+sz, 0, sz);
-        }
+  char* bfr = calloc(sizeof(char), 1024);
+  int z = 1;
+  for (; (c = input[z]) != '"'; z++) {
+    bfr[z-1] = c;
+    if (z % 1023 == 0) {
+      int sz = strlen(bfr)*sizeof(char);
+      bfr = realloc(bfr, sz + 1024);
+      memset(bfr+sz, 0, sz);
     }
-    node_t* node = mkNodeString(bfr);
-    free(bfr);
+  }
+  node_t* node = mkNodeString(bfr);
+  free(bfr);
 
-    res->consumed = z + 1;
-    res->value = node;
-    return res;
+  res->consumed = z + 1;
+  res->value = node;
+  return res;
 }
 
 parser_result_t* parseSymbol(const char* input) {
-    parser_result_t* res = malloc(sizeof(parser_result_t));
-    char c;
+  parser_result_t* res = malloc(sizeof(parser_result_t));
+  char c;
 
-    char* bfr = calloc(sizeof(char), 1024);
-    int z = 0;
-    for (; (c = input[z]) != ' ' && (c = input[z]) != '\0'; z++) {
-        bfr[z] = c;
-        bfr[z+1] = '\0';
-        if (z != 0 && z % 1024 == 0) {
-            int sz = strlen(bfr)*sizeof(char);
-            bfr = realloc(bfr, sz + 1024);
-            memset(bfr+sz, 0, sz);
-        }
+  char* bfr = calloc(sizeof(char), 1024);
+  int z = 0;
+  for (; (c = input[z]) != ' ' && (c = input[z]) != '\0'; z++) {
+    bfr[z] = c;
+    bfr[z+1] = '\0';
+    if (z != 0 && z % 1024 == 0) {
+      int sz = strlen(bfr)*sizeof(char);
+      bfr = realloc(bfr, sz + 1024);
+      memset(bfr+sz, 0, sz);
     }
-    node_t* node = mkNodeSymbol(bfr);
-    free(bfr);
+  }
+  node_t* node = mkNodeSymbol(bfr);
+  free(bfr);
 
-    res->consumed = z;
-    res->value = node;
-    return res;
+  res->consumed = z;
+  res->value = node;
+  return res;
 }
 
 parser_result_t* parse(const char*);
 
 int findClosingParenIndex(const char* input) {
-    char c;
-    int i = 0;
-    int balance = 0;
-    while ((c = input[i++]) != 0) {
-        if (c == '(') {
-            balance++;
-        } else if (c == ')') {
-            balance--;
-            if (balance == 0) return i;
-        }
+  char c;
+  int i = 0;
+  int balance = 0;
+  while ((c = input[i++]) != 0) {
+    if (c == '(') {
+      balance++;
+    } else if (c == ')') {
+      balance--;
+      if (balance == 0) return i;
     }
+  }
 
-    return -1;
+  return -1;
 }
 
 parser_result_t* parseList(const char* input) {
-    parser_result_t* res = malloc(sizeof(parser_result_t));
+  parser_result_t* res = malloc(sizeof(parser_result_t));
 
-    int end = findClosingParenIndex(input);
+  int end = findClosingParenIndex(input);
 
-    char* bfr = calloc(sizeof(char), end);
-    memcpy(bfr, input+1, end-2);
-    bfr[end-1] = '\0';
+  char* bfr = calloc(sizeof(char), end);
+  memcpy(bfr, input+1, end-2);
+  bfr[end-1] = '\0';
 
-    parser_result_t* currentNode = NULL;
-    ast_node_list_t* rootNode = &nil;
-    int bfrOffset = 0;
-    while ((currentNode = parse(bfr+bfrOffset))->consumed != 0) {
-        bfrOffset += currentNode->consumed;
-        rootNode = append(currentNode->value, rootNode);
-    }
-    if (currentNode->consumed == 0) free(currentNode);
-    free(bfr);
+  parser_result_t* currentNode = NULL;
+  ast_node_list_t* rootNode = &nil;
+  int bfrOffset = 0;
+  while ((currentNode = parse(bfr+bfrOffset))->consumed != 0) {
+    bfrOffset += currentNode->consumed;
+    rootNode = append(currentNode->value, rootNode);
+  }
+  if (currentNode->consumed == 0) free(currentNode);
+  free(bfr);
 
-    node_t* node = wrapNodeList(rootNode);
+  node_t* node = wrapNodeList(rootNode);
 
-    res->consumed = end;
-    res->value = node;
-    return res;
+  res->consumed = end;
+  res->value = node;
+  return res;
 }
 
 parser_result_t* parseQuoted(const char* input) {
-    parser_result_t* res = malloc(sizeof(parser_result_t));
-    parser_result_t* child = parse(input+1);
+  parser_result_t* res = malloc(sizeof(parser_result_t));
+  parser_result_t* child = parse(input+1);
 
-    res->consumed = child->consumed+1;
-    res->value = mkQuoted(child->value);
+  res->consumed = child->consumed+1;
+  res->value = mkQuoted(child->value);
 
-    free(child);
-    return res;
+  free(child);
+  return res;
 }
 
 parser_result_t* parse(const char* input) {
-    // printf("entering parse '%s' (%lu)\n", input, strlen(input));
-    if (input == NULL || strlen(input) == 0) {
-        parser_result_t* pr = malloc(sizeof(parser_result_t));
-        pr->consumed = 0;
-        pr->value = wrapNodeList(&nil);
-        return pr;
+  // printf("entering parse '%s' (%lu)\n", input, strlen(input));
+  if (input == NULL || strlen(input) == 0) {
+    parser_result_t* pr = malloc(sizeof(parser_result_t));
+    pr->consumed = 0;
+    pr->value = wrapNodeList(&nil);
+    return pr;
+  }
+
+  char c = input[0];
+  int i = 0;
+  do {
+    parser_result_t* res;
+    if (isdigit(c) || c == '-') {
+      res = parseNumber(input);
+    } else if (c == '"') {
+      res = parseString(input);
+    } else if (c == '(') {
+      res = parseList(input);
+    } else if (c == '\'') {
+      res = parseQuoted(input);
+    } else {
+      res = parseSymbol(input);
     }
 
-    char c = input[0];
-    int i = 0;
-    do {
-        parser_result_t* res; 
-        if (isdigit(c) || c == '-') {
-            res = parseNumber(input);
-        } else if (c == '"') {
-            res = parseString(input);
-        } else if (c == '(') {
-            res = parseList(input);
-        } else if (c == '\'') {
-            res = parseQuoted(input);
-        } else {
-            res = parseSymbol(input);
-        }
+    i += res->consumed;
+    c = input[i];
 
-        i += res->consumed;
-        c = input[i];
+    if (!isspace(c) && c != (int)NULL) {
+      printf("Syntax error at %d (%c)\n", i, c);
+      free(res);
 
-        if (!isspace(c) && c != (int)NULL) {
-            printf("Syntax error at %d (%c)\n", i, c);
-            free(res);
+      parser_result_t* pr = malloc(sizeof(parser_result_t));
+      pr->consumed = 0;
+      pr->value = wrapNodeList(&nil);
+      return pr;
+    }
 
-            parser_result_t* pr = malloc(sizeof(parser_result_t));
-            pr->consumed = 0;
-            pr->value = wrapNodeList(&nil);
-            return pr;
-        }
+    while(isspace(input[i])) {
+      ++i;
+    }
 
-        while(isspace(input[i])) {
-            ++i;
-        }
+    res->consumed = i;
 
-        res->consumed = i;
-
-        return res;
-    } while ((c = input[i++]) != (int)NULL);
+    return res;
+  } while ((c = input[i++]) != (int)NULL);
 }
